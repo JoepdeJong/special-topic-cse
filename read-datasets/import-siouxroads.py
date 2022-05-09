392d62d9b00796d079bf1f401720717b08215f48
Anne-Fleur Dijkhorst <75758696+adijkhorst@users.noreply.github.com>
2022-05-09 11:04:55 +0200

    Update import-siouxroads.py
    Changed import file to a function that can be imported

--- read-datasets/import-siouxroads.py
+++ read-datasets/import-siouxroads.py
@@ -4,29 +4,32 @@ import matplotlib.pyplot as plt
 
 plt.close('all')
 
-fileName = 'SiouxFalls_net.csv'
+def import_siouxroads():
+    fileName = 'SiouxFalls_net.csv'
+    
+    a = np.loadtxt(fileName, delimiter = ',', skiprows = 1)
+    a = a[:, 1:3].astype(np.int32) #a is now an array with each row a link between two nodes in the network
+    n = 24
+    
+    ### add 24 nodes to a directed graph
+    G = nx.DiGraph()
+    G.add_nodes_from(np.arange(1,n))
+    
+    
+    ### add edges to the directed graph
+    edges = tuple([tuple(e) for e in a]) #forward edges as tuples
+    backedges = tuple([tuple(e) for e in np.flip(a, axis=1)]) #backward edges as tuples
+    
+    G.add_edges_from(edges)
+    G.add_edges_from(backedges)
+    
+    ### compute adjacency matrix of G
+    A = nx.adjacency_matrix(G)
+    A = A.todense()
+    return G, A
 
-a = np.loadtxt(fileName, delimiter = ',', skiprows = 1)
-a = a[:, 1:3].astype(np.int32) #a is now an array with each row a link between two nodes in the network
-
-
-### add 24 nodes to a directed graph
-G = nx.DiGraph()
-G.add_nodes_from(np.arange(1,24))
-
-
-### add edges to the directed graph
-edges = tuple([tuple(e) for e in a]) #forward edges as tuples
-backedges = tuple([tuple(e) for e in np.flip(a, axis=1)]) #backward edges as tuples
-
-G.add_edges_from(edges)
-G.add_edges_from(backedges)
-
-### compute adjacency matrix of G
-A = nx.adjacency_matrix(G)
-A = A.todense()
-
-### draw the graph with node labels
-plt.figure()
-nx.draw(G, with_labels= True)
-plt.show()
+def drawgraph(G):
+    ### draw the graph with node labels
+    plt.figure()
+    nx.draw(G, with_labels= True)
+    plt.show()
