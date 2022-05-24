70d8ae608eba4ecc11d0ec018d607fabde498d94
Joep de Jong <j.dejong-16@student.tudelft.nl>
2022-05-24 14:53:23 +0200

    Add prices model

--- models/prices_model.py
+++ models/prices_model.py
@@ -1,13 +1,15 @@
+from matplotlib import pyplot as plt
 from networkx import nx
 import numpy as np
 import random
 
-def prices_graph(n, m, k0 = 1):
+def prices_model(n, m, k0 = 1):
     """
     Prices model. Implementation according to wikipedia, but it doesn't work. xd
     """
     in_degrees = np.zeros(n, dtype=int)
-    out_degrees = np.zeros(n, dtype=int)
+    # in_degrees = np.zeros(n, dtype=int)
+    # out_degrees = np.zeros(n, dtype=int)
     
     # Initialize graph with 1 node
     G = nx.DiGraph()
@@ -15,26 +17,51 @@ def prices_graph(n, m, k0 = 1):
 
     n_nodes = 1
 
+    # At each time step
     while n_nodes < n:
-        # Add a new node
+        # A new node j
         G.add_node(n_nodes)
 
-        # Add citations
-        for i in range(n_nodes):
-            # Create m directed edges to m (distinct) already present nodes where the likelihood of joining to a node i is proportional to its in-degree.
-            probability = (in_degrees[i] + k0)*p_k(m, k0, in_degrees[i])/(m+k0)
-            print(probability)
-            if random.random() < probability:
-                # Create directed edges (citation)
-                G.add_edge(n_nodes, i)
-                out_degrees[n_nodes] += 1
-                in_degrees[i] += 1
+        joined = []
+        degrees = in_degrees.copy()
+
+        # Creates m directed edges to m (distinct) already present nodes.
+        for k in range(m):
+
+            # Divide the factor k0 over the nodes that are the current node has not joined.
+            k0s = np.zeros(n)
+            k0s[:n_nodes] = k0/(n_nodes - len(joined))
+            k0s[joined] = 0
+            degrees[joined] = 0
+            
+            # Choose a node i with probability proportional to its in-degree and the factor k0.
+            p = (degrees + k0s)/(sum(degrees) + 1)
+
+
+            # Find node i by looping over the probabilities.
+            randp = random.random()
+            tot = 0
+            i = -1
+            while tot < randp:
+                i += 1
+                tot += p[i]
+
+            # Create directed edges (citation)
+            G.add_edge(n_nodes, i)
+            in_degrees[i] += 1
+            joined += [i]
+
+            print('Add edge', k+1, 'of', m, 'to node', i+1)
+
+            # Break when n_nodes is less than m.
+            if len(joined) == n_nodes:
+                break
 
         n_nodes += 1
 
-
     return G
 
-def p_k(m, k0, k):
-    # return (1+1/m)*np.random.beta(k+1, 2+1/m)
-    return (m+k0)/(m*(k0 + 1) + k0)*(np.random.beta(k+k0, 2+k0/m))/(np.random.beta(k0, 2+k0/m))
+if __name__ == '__main__':
+    prices_graph = prices_model2(20, 1, 1)
+    nx.draw(prices_graph, with_labels=True)
+    plt.show()
