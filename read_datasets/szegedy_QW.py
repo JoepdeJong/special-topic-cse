b570c5a20a84c4e6dbcd8c5cfb52859550be4a33
Antón <antonr.o.98@gmail.com>
2022-05-25 11:56:27 +0200

    fixes

--- read_datasets/szegedy_QW.py
+++ read_datasets/szegedy_QW.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.sparse as sp
 # transition matrix (copied from entropy.py)
 def transmatrix(A):
-    n = len(A)
+    n = A.shape[0]
     outdeg = np.zeros(n)
     for i in range(n):
         outdeg[i] = np.sum(A[i,:])
@@ -16,31 +16,37 @@ def transmatrix(A):
     return T.T
 
 
-def SQW_unitary(P):
+def SQW_unitary(A):
     '''
-    P is the stochastic matrix representing the Markov Chain
+    A is the adjacency matrix of a graph
+
     Returns: Unitary matrix corresponding to the Szegedy Quantum Walk operator
     '''
-    N = len(P)
+
+     
+    P = sp.csr_matrix(transmatrix(A)) # P is the stochastic matrix representing the Markov Chain
+
+    N = P.shape[0]
 
     b_vec = sp.csr_matrix((N,N)) #canonical basis vectors
 
     for i in range(N):
-        b_vec[:,i] = (csr_matrix(([1],([i],[0])),shape = (N,1)))
+        b_vec[:,i] = (sp.csr_matrix(([1],([i],[0])),shape = (N,1)))
 
     psi = []
-    sqrt_edges = np.sqrt(P).T@(b_vec)
+    P_sqr = np.sqrt(P)
     for j in range(N):
         psi.append(sp.kron(b_vec[:,j],P_sqr[j,:].T))
 
-    N2 = N*N
+    N2 = N*N 
     PI = sp.csr_matrix((N2,N2))
     for j in range(N):
         PI += psi[j]*psi[j].T
-
+      
     S = sp.csr_matrix((N2,N2))
     for j in range(N):
         for k in range(N):
-            S += sp.kron(b_vec[j],b_vec[k])*sp.kron(b_vec[k],b_vec[j]).T
+            S += sp.kron(b_vec[:,j],b_vec[:,k])*sp.kron(b_vec[:,k],b_vec[:,j]).T
+
 
     return S*(2*PI-sp.eye(N2))
