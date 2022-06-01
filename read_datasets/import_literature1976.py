63c1e56f976ae2010f26375da0b2c2a62050005e
adijkhorst <anne-fleur@dijkhorst.nl>
2022-06-01 16:47:06 +0200

    small fix to only return G from import file (instead of G and A)

--- read_datasets/import_literature1976.py
+++ read_datasets/import_literature1976.py
@@ -38,7 +38,7 @@ def import_draw_literature(draw=False):
 	A = A.todense()
 
 	if not draw:
-		return G, A
+		return G
 
 	### draw the graph with node labels
 	plt.close('all')
@@ -46,4 +46,4 @@ def import_draw_literature(draw=False):
 	nx.draw(G, with_labels= True)
 	plt.show()
 
-	return G, A
+	return G
