31d2a48d83461ef9570f508c76505475aac656b3
Joep de Jong <j.dejong-16@student.tudelft.nl>
2022-06-06 13:48:58 +0200

    Implement the mutation loop

---
+++ mutation_loop.py
@@ -0,0 +1,82 @@
+from matplotlib import pyplot as plt
+import networkx as nx
+import numpy as np
+import pandas as pd
+
+from mutators import *
+from read_datasets.entropy import *
+from read_datasets.hierarchy import *
+
+filename = 'dataset/prices_model_seed1313_m3_k01_initial_nodes1.csv'
+
+data = pd.read_csv(filename)
+number_of_mutations = 20
+
+n_nodes = data['n'].unique()
+n_nodes = [50]
+n_iterations = data['iteration'].max()
+reciprocal_threshold = data['reciprocal_threshold'].unique()
+
+mutators = [node_removal, edge_removal, edge_reversal, edge_addition]
+mutation_steps = [1, 5, 10]
+
+# Assume m, k0 and initial_nodes are the same for all n
+k0 = 1
+m = 3
+initial_nodes = 1
+
+
+output = pd.DataFrame(columns=['n', 'iteration', 'mutation', 'mutation_number', 'step', 'reciprocal_threshold', 'df', 'entropy', 'normalized_entropy', 'spectral_scaling', 'spectral_gap', 'root_is_leader'])
+n_fields = len(['df', 'entropy', 'normalized_entropy', 'spectral_scaling', 'spectral_gap', 'root_is_leader'])
+
+for n in n_nodes:
+    for r in reciprocal_threshold:
+        data_n_p = data[(data['n'] == n) & (data['reciprocal_threshold'] == r)]
+
+        # values = np.zeros((len(mutators), n_fields, number_of_mutations, len(mutation_steps)))
+
+        for index, row in data_n_p.iterrows():
+            k = row['iteration']
+
+            # Import graph from file
+            graph = nx.read_edgelist('dataset/'+row['filename']+'.txt', data=False, create_using=nx.DiGraph())
+
+            # Mutate graph
+            for mutator in mutators:
+                for i in range(number_of_mutations):
+                    # Copy the graph
+                    graph_copy = graph.copy()
+
+                    k = 0
+                    for s in range(len(mutation_steps)):
+                        step = mutation_steps[s]
+                        graph_copy = mutator(graph_copy, step - k)
+                        k += step
+
+                        A = nx.adjacency_matrix(graph_copy).todense()
+
+                        # Calculate the measures
+                        df = Henrici(A)
+                        h = entropy2(A).real
+                        h_norm = h/entropy2((A+A.T)/2).real
+                        spectral_scaling, spectral_gap = spectralScalingMeasure(A)
+
+                        # The only leader node in the prices model can be the root node
+                        root_is_leader = is_leader_node(graph_copy, 0)
+
+                        # Save the measures
+                        # values[m, :, i, s] = [df, h, h_norm, spectral_scaling, spectral_gap, root_is_leader]
+
+                        output.loc[len(output)] = [n, k, mutator.__name__, r, i, s, df, h, h_norm, spectral_scaling, spectral_gap, root_is_leader]
+                    del graph_copy
+        # del values
+        print('n = ' + str(n) + ', p = ' + str(r))
+
+data.to_csv('mutations_m' + str(m) + '_k0' + str(k0) + '_initial_nodes' + str(initial_nodes)+'.csv', index=False)
+
+
+            
+
+            # print(data_n_p_k)
+    # data_n_p = data[data['n'] == n].groupby('reciprocal_threshold')
+    
