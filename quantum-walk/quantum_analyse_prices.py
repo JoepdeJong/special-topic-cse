5046967716af4d6f49b4ede8b71d45fabea24b87
Antón <antonr.o.98@gmail.com>
2022-06-08 15:40:40 +0200

    translate folder it0

--- quantum-walk/quantum_analyse_prices.py
+++ quantum-walk/quantum_analyse_prices.py
@@ -2,6 +2,8 @@ from szegedy_QW import average_dist_U2
 import os
 import networkx as nx
 import numpy as np
+import scipy.sparse as sp
+from tqdm import tqdm
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -36,6 +38,33 @@ def get_dist(n_iteration,N_steps):
 		count+=1
 	
 	return
+
+def transmatrix(A):
+    n = A.shape[0]
+    outdeg = sp.csr_matrix((n,1))
+    for i in range(n):
+        outdeg[i,0] = sp.csr_matrix.sum(A.getcol(i))
+
+    T = sp.csr_matrix((n,n))
+    for j in range(n):
+        if outdeg[j,0] > 0:
+            T[j,:] = A.getrow(j)/outdeg[j,0]
+        else:
+            T[j,j] = 1
+    return T.T
+
+
+def eval_entropy(T,p_dist):
+
+def get_entropy(n_iteration):
+
+	datasets = graph_data_list(n_iteration)
+
+	#Build graph
+	for f in datasets:
+		G = nx.DiGraph()
+
+		G.add_edges_from(np.loadtxt('../dataset/'+str(f)))
 
 
 
