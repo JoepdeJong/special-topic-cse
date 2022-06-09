00bdde560e501e39f3b91ca6b055d6c402518175
Antón <antonr.o.98@gmail.com>
2022-06-09 17:03:31 +0200

    hjj

--- quantum-walk/quantum_analyse_prices.py
+++ quantum-walk/quantum_analyse_prices.py
@@ -31,7 +31,7 @@ def get_dist(n_iteration,N_steps):
 
 		p_av = average_dist_U2(G,N_steps)
 
-		np.save(os.path.join('results_'+str(N_steps)+'_steps_it'+str(n_iteration),str(f[:-4])),p_av)
+		np.save(os.path.join('new_results_'+str(N_steps)+'_steps_it'+str(n_iteration),str(f[:-4])),p_av.todense())
 
 		#track progress in simulation
 		print(str(count+1)+total,end='\r')
@@ -55,16 +55,146 @@ def transmatrix(A):
 
 
 def eval_entropy(T,p_dist):
+	n = T.shape[0]
+	h = 0
+	for i in range(n):
+		for j in range(n):
+			if T[i,j] > 0:
+				h -= T[i,j]*p_dist[j]*np.log(T[i,j])
+	return h
+
 
 def get_entropy(n_iteration):
 
 	datasets = graph_data_list(n_iteration)
 
-	#Build graph
-	for f in datasets:
+	#assemble folder name
+	foldername_p_dist = 'results_650_steps_it'+str(n_iteration)+'/'
+
+	#build array to store entropies
+	# first row for 20 node graphs
+	# second row for 50 node graphs
+	# third row for 100 node graphs
+	entropy_array = np.zeros((3,25))
+
+	#build array to store reciprocal thresholds
+	# first row for 20 node graphs
+	# second row for 50 node graphs
+	# third row for 100 node graphs
+	rec_threshold_array = np.zeros((3,25))
+
+
+ 
+	i20 = 0
+	i50 = 0
+	i100 = 0
+	for f in tqdm(datasets):
+		#Build graph
 		G = nx.DiGraph()
 
 		G.add_edges_from(np.loadtxt('../dataset/'+str(f)))
+
+		T = transmatrix(nx.adjacency_matrix(G))
+
+		#load prob dist, but it got stored in a weird
+		p_dist = np.load(foldername_p_dist+f[:-4]+'.npy')
+
+		if f[14:17] == '100':
+			entropy_array[2,i100] = eval_entropy(T,p_dist)
+			
+			try:
+				rec_threshold_array[2,i100] = float(f[-19:-15])
+			except ValueError:
+				rec_threshold_array[2,i100] = float(f[-18:-15])
+
+			i100 += 1
+
+		elif f[14:16] == '50':
+			entropy_array[1,i50] = eval_entropy(T,p_dist)
+			
+			try:
+				rec_threshold_array[1,i50] = float(f[-19:-15])
+			except ValueError:
+				rec_threshold_array[1,i50] = float(f[-18:-15])
+
+			i50 += 1
+
+		else:
+			entropy_array[0,i20] = eval_entropy(T,p_dist)
+			
+			try:
+				rec_threshold_array[0,i20] = float(f[-19:-15])
+			except ValueError:
+				rec_threshold_array[0,i20] = float(f[-18:-15])
+
+			i20 += 1
+
+	np.save('entropy_array_it'+str(n_iteration),entropy_array)
+	np.save('rec_threshold_array_it'+str(n_iteration),rec_threshold_array)
+
+def get_normalized_entropy(n_iteration):
+
+	datasets = graph_data_list(n_iteration)
+
+	#assemble folder name
+	foldername_p_dist = 'new_results_650_steps_it'+str(n_iteration)+'/'
+
+	#build array to store entropies
+	# first row for 20 node graphs
+	# second row for 50 node graphs
+	# third row for 100 node graphs
+	entropy_array = np.zeros((3,25))
+
+	#build array to store reciprocal thresholds
+	# first row for 20 node graphs
+	# second row for 50 node graphs
+	# third row for 100 node graphs
+	rec_threshold_array = np.zeros((3,25))
+
+
+ 
+	i20 = 0
+	i50 = 0
+	i100 = 0
+	for f in tqdm(datasets):
+		#Build graph
+		G = nx.DiGraph()
+
+		G.add_edges_from(np.loadtxt('../dataset/'+str(f)))
+
+		A = nx.adjacency_matrix(G)
+
+		T = transmatrix(A)
+
+		T_sim = transmatrix((A+A.T)/2)
+
+		#load prob dist
+		p_dist = np.load(foldername_p_dist+f[:-4]+'.npy')
+
+		if f[14:17] == '100':
+			p_dist_sim = 1/100*np.ones(100)
+			entropy_array[2,i100] = eval_entropy(T,p_dist)/eval_entropy(T_sim,p_dist_sim)
+
+			i100 += 1
+
+		elif f[14:16] == '50':
+			p_dist_sim = 1/50*np.ones(50)
+			entropy_array[1,i50] = eval_entropy(T,p_dist)/eval_entropy(T_sim,p_dist_sim)
+
+			i50 += 1
+
+		else:
+			p_dist_sim = 1/20*np.ones(20)
+			entropy_array[0,i20] = eval_entropy(T,p_dist)/eval_entropy(T_sim,p_dist_sim)
+	
+			i20 += 1
+
+	np.save('new_normalized_entropy_array_it'+str(n_iteration),entropy_array)
+
+	return 
+
+
+
 
 
 
--- quantum-walk/szegedy_QW.py
+++ quantum-walk/szegedy_QW.py
@@ -17,7 +17,21 @@ def transmatrix(A):
         else:
             T[j,j] = 1
     return T.T
+'''
+def transmatrix_new(A):
+    n = A.shape[0]
+    outdeg = sp.csr_matrix((n,1))
+    for i in range(n):
+        outdeg[i,0] = sp.csr_matrix.sum(A.getcol(i))
 
+    T = sp.csr_matrix((n,n))
+    for j in range(n):
+        if outdeg[j,0] > 0:
+            T[j,:] = A.getrow(j)/outdeg[j,0]
+        else:
+            T[j,j] = 1
+    return T.T
+'''
 
 def SQW_unitary(A):
     '''
@@ -52,7 +66,43 @@ def SQW_unitary(A):
             S += sp.kron(b_vec[:,j],b_vec[:,k])*sp.kron(b_vec[:,k],b_vec[:,j]).T
 
 
-    return S*(2*PI-sp.eye(N2)),b_vec
+    return S*(2*PI-sp.eye(N2)),b_vec,psi
+
+def SQW_unitary_new(A):
+    '''
+    A is the adjacency matrix of a graph
+
+    Returns: Unitary matrix corresponding to the Szegedy Quantum Walk operator
+    '''
+
+     
+    P = sp.csr_matrix(transmatrix(A)) # P is the stochastic matrix representing the Markov Chain
+
+    N = P.shape[0]
+
+    b_vec = sp.csr_matrix((N,N)) #canonical basis vectors
+
+    for i in range(N):
+        b_vec[:,i] = (sp.csr_matrix(([1],([i],[0])),shape = (N,1)))
+
+    N2 = N*N
+    psi = sp.csr_matrix((N2,N))
+    P_sqr = np.sqrt(P)
+    for j in range(N):
+        psi[:,j] = sp.kron(b_vec[:,j],P_sqr[:,j])
+
+     
+    PI = sp.csr_matrix((N2,N2))
+    for j in range(N):
+        PI += psi[:,j]*psi[:,j].T
+      
+    S = sp.csr_matrix((N2,N2))
+    for j in range(N):
+        for k in range(N):
+            S += sp.kron(b_vec[:,j],b_vec[:,k])*sp.kron(b_vec[:,k],b_vec[:,j]).T
+
+
+    return S*(2*PI-sp.eye(N2)),b_vec,psi
 
 def average_dist(G,N_steps):
 
@@ -60,12 +110,14 @@ def average_dist(G,N_steps):
 
     N = G.number_of_nodes()
 
-    U,b_vec = SQW_unitary(A) # unitary that sets the evolution of the QW
+    U , b_vec , psi = SQW_unitary_new(A) # unitary that sets the evolution of the QW
+    
 
     # set initial state to uniform one
     n = U.shape[0]
 
-    alpha = sp.csr_matrix(np.ones(n)/np.sqrt(n)).T
+    #alpha = sp.csr_matrix(np.ones(n)/np.sqrt(n)).T
+    alpha = sp.csr_matrix(sp.csr_matrix.sum(psi,1)/np.sqrt(psi.shape[1]))
 
     # set initial distribution from initial state
     p_sum = sp.csr_matrix((N,1))
@@ -73,11 +125,12 @@ def average_dist(G,N_steps):
     for k in range(N):
         p_sum += (sp.kron(b_vec,b_vec[:,k]).T*alpha).power(2)
 
+    projectors = [sp.kron(b_vec,b_vec[:,k]).T for k in range(N)]
+
     #perform N_steps of the SQW and calculate temportal average of p(j,t) = sum_k |<jk|alpha(t)>|^2
     for i in range(N_steps):
         alpha = U*alpha
-        for k in range(N):
-            p_sum += (sp.kron(b_vec,b_vec[:,k]).T*alpha).power(2)
+        p_sum += np.sum(np.array([(projectors[k]*alpha).power(2) for k in range(N)]))
 
     return p_sum/N_steps
 
@@ -88,7 +141,7 @@ def average_dist2(G,N_steps):
 
     N = G.number_of_nodes()
 
-    U, b_vec = SQW_unitary(A) # unitary that sets the evolution of the QW
+    U, b_vec , psi = SQW_unitary(A) # unitary that sets the evolution of the QW
     U2 = U*U
 
     b_vec = sp.csr_matrix((N,N)) #canonical basis vectors
@@ -123,13 +176,14 @@ def average_dist_U2(G,N_steps):
 
     N = G.number_of_nodes()
 
-    U,b_vec = SQW_unitary(A) # unitary that sets the evolution of the QW
+    U , b_vec , psi = SQW_unitary_new(A) # unitary that sets the evolution of the QW
     U2 = U*U
 
     # set initial state to uniform one
     n = U.shape[0]
 
-    alpha = sp.csr_matrix(np.ones(n)/np.sqrt(n)).T
+    #alpha = sp.csr_matrix(np.ones(n)/np.sqrt(n)).T
+    alpha = sp.csr_matrix(sp.csr_matrix.sum(psi,1)/np.sqrt(psi.shape[1]))
 
     # set initial distribution from initial state
     p_sum = sp.csr_matrix((N,1))
