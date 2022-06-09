ba4a3ea647bdffd96c5090a79f797f7e3527ec59
adijkhorst <anne-fleur@dijkhorst.nl>
2022-06-09 18:40:58 +0200

    created graphs showing the difference in probability distributions on the nodes

--- quantum-walk/probability_visualisation.py
+++ quantum-walk/probability_visualisation.py
@@ -32,11 +32,11 @@ def draw_classical_prob(G):
 def draw_quantum_prob(G, p):
 	stat = p
 
-	plt.figure()
+	plt.figure(figsize = (18,9))
 	pos = nx.spring_layout(G, seed = 2)
-	nodes = nx.draw_networkx_nodes(G, pos, node_color = stat, cmap = plt.cm.autumn_r, vmin = 0, vmax = 1)
+	nodes = nx.draw_networkx_nodes(G, pos, node_color = stat, cmap = plt.cm.autumn_r, vmin = 0, vmax = max(abs(stat)))
 	nx.draw_networkx_edges(G, pos)
-	nx.draw_networkx_labels(G,pos)
+	nx.draw_networkx_labels(G, pos)
 	plt.colorbar(nodes)
 	plt.axis('off')
 	plt.show()
@@ -82,16 +82,30 @@ if __name__ == '__main__':
 		### build graph
 		G = nx.DiGraph()
 		G.add_edges_from(np.loadtxt('../dataset/'+str(f)))
+		G = nx.convert_node_labels_to_integers(G, first_label=0, ordering='default', label_attribute=None)
 
 		f2 = str(f[:-3] + 'npy')
+
+		### code that creates probability difference in graphs
 		p = np.load('new_results_650_steps_it1/'+f2)
 		c = draw_classic_quantum_probs(G, p, f[:-4])
-		plt.figure()
-		plt.plot(np.arange(len(G.nodes())), p, label = 'quantum')
-		plt.plot(np.arange(len(G.nodes())), c, label = 'classic')
-		plt.title('p = '+str(a[i]))
-		plt.legend()
-		plt.savefig("probability_results_plots/"+str(f[:-4])+".svg", format = 'svg', dpi=300)
+
+		draw_quantum_prob(G, np.abs(p.flatten()-c))
+		plt.savefig("probability_results_graphs/"+str(f[:-4])+".svg", format = 'svg', dpi=300)
 		plt.close()
+
+
+		### code that creates probability plots
+
+# 		p = np.load('new_results_650_steps_it1/'+f2)
+# 		c = draw_classic_quantum_probs(G, p, f[:-4])
+# 		plt.figure()
+# 		plt.plot(np.arange(len(G.nodes())), p, label = 'quantum')
+# 		plt.plot(np.arange(len(G.nodes())), c, label = 'classic')
+# 		plt.title('p = '+str(a[i]))
+# 		plt.legend()
+# 		plt.savefig("probability_results_plots/"+str(f[:-4])+".svg", format = 'svg', dpi=300)
+# 		plt.close()
+
 		i += 1
 
